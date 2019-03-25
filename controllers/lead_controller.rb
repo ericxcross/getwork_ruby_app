a2402318c9e91ffee28278f1e903445b52885936
ericxcross <eric.cross@sympatico.ca>
2019-03-25 17:32:35 +0000

    halfway point, get edit leads form

--- controllers/lead_controller.rb
+++ controllers/lead_controller.rb
@@ -11,8 +11,21 @@ get '/lead' do
   erb (:"/lead/index")
 end
 
+get '/lead/:id/edit' do #Edit / New Lead
+  @lead = Lead.find(params[:id])
+  @companies = Company.all()
+  @status_list = Status.all()
+  erb(:"/lead/edit")
+end
+
 get '/lead/:id' do
   @lead = Lead.find(params[:id])
   @company = @lead.company
   erb (:"/lead/show")
+end
+
+post '/lead/:id' do #Post back EDIT or NEW
+  @lead = Lead.new(params)
+  params[:id] == 0 ? @lead.save : @lead.update
+  redirect '/lead'
 end
